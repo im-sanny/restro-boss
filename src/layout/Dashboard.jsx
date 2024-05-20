0a23c32c4175f766fb8e4ed895b2a03f1473abf7
im-sanny <raselparvezsanny@gmail.com>
2024-05-20 18:30:56 +0600

    added delete method to delete data from ui and database

--- src/layout/Dashboard.jsx
+++ src/layout/Dashboard.jsx
@@ -1,35 +1,68 @@
-import { FaAddressBook, FaBookmark, FaCalendar, FaCartShopping, FaHouse, FaStar, FaWallet } from "react-icons/fa6";
+import {
+  FaBookmark,
+  FaCalendar,
+  FaCartShopping,
+  FaHouse,
+  FaSearchengin,
+  FaStar,
+  FaWallet,
+} from "react-icons/fa6";
 import { NavLink, Outlet } from "react-router-dom";
+import useCart from "../hooks/useCart";
 
 const Dashboard = () => {
+    const [cart] = useCart()
   return (
     <div className="flex">
-        {/* dashboard sidebar */}
+      {/* dashboard sidebar */}
       <div className="w-64 min-h-screen bg-orange-400">
-        <ul className="menu">
+        <ul className="menu space-y-1">
           <li>
-            <NavLink to={"/dashboard/cart"}><FaHouse></FaHouse> USER HOME</NavLink>
+            <NavLink to={"/dashboard/userHome"}>
+              <FaHouse></FaHouse> USER HOME
+            </NavLink>
           </li>
           <li>
-            <NavLink to={"/dashboard/cart"}><FaCalendar></FaCalendar> RESERVATION</NavLink>
+            <NavLink to={"/dashboard/reservation"}>
+              <FaCalendar></FaCalendar> RESERVATION
+            </NavLink>
           </li>
           <li>
-            <NavLink to={"/dashboard/cart"}><FaWallet></FaWallet> PAYMENT HISTORY</NavLink>
+            <NavLink to={"/dashboard/payment"}>
+              <FaWallet></FaWallet> PAYMENT HISTORY
+            </NavLink>
           </li>
           <li>
-            <NavLink to={"/dashboard/cart"}><FaCartShopping></FaCartShopping> My Cart</NavLink>
+            <NavLink to={"/dashboard/cart"}>
+              <FaCartShopping></FaCartShopping> My Cart ({cart.length})
+            </NavLink>
           </li>
           <li>
-            <NavLink to={"/dashboard/cart"}><FaStar></FaStar> ADD REVIEW</NavLink>
+            <NavLink to={"/dashboard/review"}>
+              <FaStar></FaStar> ADD REVIEW
+            </NavLink>
           </li>
           <li>
-            <NavLink to={"/dashboard/cart"}><FaBookmark></FaBookmark>My Bookings</NavLink>
+            <NavLink to={"/dashboard/booking"}>
+              <FaBookmark></FaBookmark>My Bookings
+            </NavLink>
           </li>
           <div className="divider"></div>
+          <li>
+            <NavLink to={"/"}>
+              <FaHouse></FaHouse>Home
+            </NavLink>
+          </li>
+          <li>
+            <NavLink to={"/"}>
+              <FaSearchengin></FaSearchengin>Home
+            </NavLink>
+          </li>
+          
         </ul>
       </div>
       {/* dashboard content */}
-      <div className="flex-1">
+      <div className="flex-1 p-8">
         <Outlet></Outlet>
       </div>
     </div>
--- src/pages/dashboard/cart/Cart.jsx
+++ src/pages/dashboard/cart/Cart.jsx
@@ -1,11 +1,95 @@
-
+import { FaTrash } from "react-icons/fa6";
+import useCart from "../../../hooks/useCart";
+import Swal from "sweetalert2";
+import useAxiosSecure from "../../../hooks/useAxiosSecure";
 
 const Cart = () => {
-    return (
-        <div>
-            <h2 className="text-5xl">My Cart</h2>
-        </div>
-    );
+  const [cart, refetch] = useCart();
+  const totalPrice = cart.reduce((total, item) => total + item.price, 0);
+  const axiosSecure = useAxiosSecure();
+  const handleDelete = (id) => {
+    Swal.fire({
+      title: "Are you sure?",
+      text: "You won't be able to revert this!",
+      icon: "warning",
+      showCancelButton: true,
+      confirmButtonColor: "#3085d6",
+      cancelButtonColor: "#d33",
+      confirmButtonText: "Yes, delete it!",
+    }).then((result) => {
+      if (result.isConfirmed) {
+        axiosSecure.delete(`/carts/${id}`).then((res) => {
+          if (res.data.deletedCount > 0) {
+            refetch()
+            Swal.fire({
+              title: "Deleted!",
+              text: "Your file has been deleted.",
+              icon: "success",
+            });
+          }
+        });
+      }
+    });
+  };
+  return (
+    <>
+      <div className="flex justify-evenly">
+        <h2 className="text-5xl">Items: {cart.length}</h2>
+        <h2 className="text-4xl"> Total Price: {totalPrice}</h2>
+        <button className="btn btn-primary">Pay</button>
+      </div>
+      <div className="overflow-x-auto">
+        <table className="table w-full">
+          {/* head */}
+          <thead>
+            <tr>
+              <th>Serial</th>
+              <th>Image</th>
+              <th>Name</th>
+              <th>Price</th>
+              <th>Action</th>
+            </tr>
+          </thead>
+          <tbody>
+            {/* row 1 */}
+            {cart.map((item, index) => (
+              <tr key={item._id}>
+                <th>{index + 1}</th>
+                <th>
+                  <div className="avatar">
+                    <div className="mask mask-squircle w-12 h-12">
+                      <img
+                        src={item.image}
+                        alt="Avatar Tailwind CSS Component"
+                      />
+                    </div>
+                  </div>
+                </th>
+
+                <td>
+                  <div className="flex items-center gap-3">
+                    <div>
+                      <div className="font-bold">{item.name}</div>
+                    </div>
+                  </div>
+                </td>
+
+                <td>{item.price}</td>
+                <th>
+                  <button
+                    onClick={() => handleDelete(item._id)}
+                    className="btn btn-ghost btn-lg text-red-600"
+                  >
+                    <FaTrash></FaTrash>
+                  </button>
+                </th>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
+    </>
+  );
 };
 
 export default Cart;
