a2e4233e93ad69bc44259e15d941672d90aa1d8f
im-sanny <raselparvezsanny@gmail.com>
2024-05-24 17:19:16 +0600

    added image and store them in imgbb by imgbb api

--- src/pages/dashboard/AddItems/AddItems.jsx
+++ src/pages/dashboard/AddItems/AddItems.jsx
@@ -1,11 +1,48 @@
 import { useForm } from "react-hook-form";
 import SectionTitle from "../../../components/sectionTitle/SectionTitle";
 import { FaUtensils } from "react-icons/fa";
+import useAxiosPublic from "../../../hooks/useAxiosPublic";
+import useAxiosSecure from "../../../hooks/useAxiosSecure";
+import Swal from "sweetalert2";
 
+const image_hosting_key = import.meta.env.VITE_IMAGE_HOSTING_KEY;
+const image_hosting_api = `https://api.imgbb.com/1/upload?key=${image_hosting_key}`;
 const AddItems = () => {
-  const { register, handleSubmit } = useForm();
-  const onSubmit = (data) => {
+  const { register, handleSubmit, reset } = useForm();
+  const axiosPublic = useAxiosPublic();
+  const axiosSecure = useAxiosSecure();
+  const onSubmit = async (data) => {
     console.log(data);
+    //image upload to imgbb and the get an url
+    const imageFile = { image: data.image[0] };
+    const res = await axiosPublic.post(image_hosting_api, imageFile, {
+      headers: {
+        "Content-Type": "multipart/form-data",
+      },
+    });
+    if (res.data.success) {
+      //now send the menu item data to the server with image url
+      const menuItem = {
+        name: data.name,
+        category: data.category,
+        price: parseFloat(data.price),
+        recipe: data.recipe,
+        image: res.data.data.display_url,
+      };
+      const menuRes = await axiosSecure.post("/menu", menuItem);
+      console.log(menuRes);
+      if (menuRes.data.insertedId) {
+        reset();
+        //show success popup
+        Swal.fire({
+          position: "top-end",
+          icon: "success",
+          title: `${data.name} is added to the menu.`,
+          showConfirmButton: false,
+          timer: 1500,
+        });
+      }
+    }
   };
   return (
     <div>
@@ -31,11 +68,12 @@ const AddItems = () => {
             {/* category */}
             <div className="form-control w-full my-6">
               <select
+                defaultValue={"default"}
                 {...register("category", { required: true })}
                 required
                 className="select select-bordered w-full "
               >
-                <option disabled selected>
+                <option disabled value={"default"}>
                   Select a category
                 </option>
                 <option value="salad">Salad</option>
