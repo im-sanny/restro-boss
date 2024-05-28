05cda7fb9c431354395bff318240e033610b565e
im-sanny <raselparvezsanny@gmail.com>
2024-05-28 19:17:02 +0600

    added payment method succesfully

--- src/pages/Payment/CheckoutForm.jsx
+++ src/pages/Payment/CheckoutForm.jsx
@@ -1,10 +1,29 @@
 import { CardElement, useElements, useStripe } from "@stripe/react-stripe-js";
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import useAxiosSecure from "../../hooks/useAxiosSecure";
+import useCart from "../../hooks/useCart";
+import useAuth from "../../hooks/useAuth";
 
 const CheckoutForm = () => {
-    const [error, setError] = useState('')
+  const { user } = useAuth();
+  const [error, setError] = useState("");
+  const [clientSecret, setClientSecret] = useState("");
+  const [transactionId, setTransactionId] = useState("");
   const stripe = useStripe();
   const elements = useElements();
+  const axiosSecure = useAxiosSecure();
+  const [cart] = useCart();
+  const totalPrice = cart.reduce((total, item) => total + item.price, 0);
+
+  useEffect(() => {
+    axiosSecure
+      .post("/create-payment-intent", { price: totalPrice })
+      .then((res) => {
+        console.log(res.data.clientSecret);
+        setClientSecret(res.data.clientSecret);
+      });
+  }, [axiosSecure, totalPrice]);
+
   const handleSubmit = async (event) => {
     event.preventDefault();
     if (!stripe || !elements) {
@@ -17,15 +36,35 @@ const CheckoutForm = () => {
     }
 
     const { error, paymentMethod } = await stripe.createPaymentMethod({
-        type:'card',
-        card
-    })
+      type: "card",
+      card,
+    });
     if (error) {
-        console.log('payment error', error);
-        setError(error.message)
-    }else{
-        console.log('payment method', paymentMethod);
-        setError('')
+      console.log("payment error", error);
+      setError(error.message);
+    } else {
+      console.log("payment method", paymentMethod);
+      setError("");
+    }
+    //confirm payment
+    const { paymentIntent, error: confirmError } =
+      await stripe.confirmCardPayment(clientSecret, {
+        payment_method: {
+          card: card,
+          billing_details: {
+            email: user?.email || "anonymous",
+            name: user?.displayName || "anonymous",
+          },
+        },
+      });
+    if (confirmError) {
+      console.log("confirm error");
+    } else {
+      console.log("payment intent", paymentIntent);
+      if (paymentIntent.status === "succeeded") {
+        console.log("transaction id", paymentIntent.id);
+        setTransactionId(paymentIntent.id);
+      }
     }
   };
   return (
@@ -46,10 +85,17 @@ const CheckoutForm = () => {
           },
         }}
       />
-      <button className="btn btn-sm btn-primary mt-5" type="submit" disabled={!stripe}>
+      <button
+        className="btn btn-sm btn-primary mt-5"
+        type="submit"
+        disabled={!stripe || !clientSecret}
+      >
         Pay
       </button>
       <p className="text-red-600">{error}</p>
+      {transactionId && (
+        <p className="text-green-600">Your transactionId {transactionId}</p>
+      )}
     </form>
   );
 };
