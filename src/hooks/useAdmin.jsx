00e8b09efdfe1209fe935d2459d84cdbd79ab048
im-sanny <raselparvezsanny@gmail.com>
2024-05-31 18:04:47 +0600

    chart related issue solved

--- src/hooks/useAdmin.jsx
+++ src/hooks/useAdmin.jsx
@@ -9,7 +9,7 @@ const useAdmin = () => {
         queryKey:[user?.email, 'isAdmin'],
         enabled: !loading,
         queryFn: async() =>{
-            console.log('asking or checking is admin', user);
+            // console.log('asking or checking is admin', user);
             const res = await axiosSecure.get(`/user/admin/${user.email}`)
             return res.data?.admin;
         }
