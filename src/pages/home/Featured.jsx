455094d07ee200ba06b1157117817e3cba539f2c
im-sanny <raselparvezsanny@gmail.com>
2024-09-07 20:06:42 +0600

    update: updated featured section design

--- src/pages/home/Featured.jsx
+++ src/pages/home/Featured.jsx
@@ -1,28 +1,43 @@
 import SectionTitle from "../../components/sectionTitle/SectionTitle";
 import featured from "../../assets/home/featured.jpg";
-import './Featured.css'
+import "./Featured.css";
+import { Link } from "react-router-dom";
 
 const Featured = () => {
   return (
-    <div className="featured-item bg-fixed text-white pt-5 my-20">
+    <div className="featured-item bg-fixed text-white  my-20">
+      {/* Section Title */}
       <SectionTitle
         heading={"Check it out"}
         subHeading={"Featured Item"}
       ></SectionTitle>
-      <div className="flex justify-center items-center bg-slate-500 bg-opacity-60 pb-20 pt-12 px-32">
-        <div className="">
-          <img src={featured} alt="" />
+
+      {/* Content Section */}
+      <div className="flex flex-col md:flex-row justify-center items-center bg-gray-800 bg-opacity-60 p-10 md:px-32 space-y-8 md:space-y-0 md:space-x-10">
+        {/* Featured Image */}
+        <div className="md:w-1/2">
+          <img
+            src={featured}
+            alt="Featured Item"
+            className="rounded-lg shadow-lg"
+          />
         </div>
-        <div className="md:ml-10">
-          <p>Aug 20, 2039</p>
-          <p className="uppercase">Where can i get some?</p>
-          <p>
+
+        {/* Featured Text */}
+        <div className="md:w-1/2 text-center md:text-left space-y-4">
+          <p className="text-gray-300">Aug 20, 2039</p>
+          <h2 className="text-2xl uppercase font-bold">
+            Where can I get some?
+          </h2>
+          <p className="text-gray-300">
             Lorem ipsum dolor sit amet consectetur adipisicing elit.
             Exercitationem ipsum illum ipsa possimus tenetur voluptate eum
             placeat cumque iste provident! Officia unde fuga enim sit. Sint
             dolores explicabo saepe adipisci!
           </p>
-          <button className="btn btn-outline border-0 border-b-4 border-black mt-4">Order Now</button>
+          <button className="btn btn-outline border-1 border-b-4 border-yellow-500 text-yellow-500 hover:bg-yellow-500 hover:text-white mt-4 transition-all duration-300 ease-in-out">
+            <Link to={"/menu"}>Order Now</Link>
+          </button>
         </div>
       </div>
     </div>
